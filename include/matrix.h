d87f5d7352cdc22e959edb8cb63b2f527793863c
zhthean <zhen.hui.thean@outlook.com>
2025-03-09 13:47:35 +0800

    Add pre-commit and workflows

--- include/matrix.h
+++ include/matrix.h
@@ -1,72 +1,65 @@
 ﻿#ifndef MATRIX_H
 #define MATRIX_H
 
-#include <vector>
 #include <iostream>
 #include <type_traits>
+#include <vector>
 
+#define INSTANTIATION_MATRIX_CONSTRUCTOR(Type1, Type2) template Matrix<Type1>::Matrix(const Matrix<Type2> &);
 
-#define INSTANTIATION_MATRIX_FUNCTIONS(Type1, Type2) \
-	template Matrix<Type1>::Matrix(const Matrix<Type2>&); \
-    template auto Matrix<Type1>::operator+(const Matrix<Type2>&) const->Matrix<std::common_type_t<Type1, Type2>>; \
-    template auto Matrix<Type1>::operator-(const Matrix<Type2>&) const->Matrix<std::common_type_t<Type1, Type2>>; \
-    template auto Matrix<Type1>::operator*(const Matrix<Type2>&) const->Matrix<std::common_type_t<Type1, Type2>>; \
-    template auto Matrix<Type1>::operator*(const Type2&) const->Matrix<std::common_type_t<Type1, Type2>>; \
-    template auto Matrix<Type1>::element_wise_product(const Matrix<Type2>&) const->Matrix<std::common_type_t<Type1, Type2>>; \
-    template auto operator*(const Type1&, const Matrix<Type2>&)->Matrix<std::common_type_t<Type1, Type2>>;
+#define INSTANTIATION_MATRIX_FUNCTIONS(Type1, Type2)                                                                   \
+  template auto Matrix<Type1>::operator+(const Matrix<Type2> &) const -> Matrix<std::common_type_t<Type1, Type2>>;     \
+  template auto Matrix<Type1>::operator-(const Matrix<Type2> &) const -> Matrix<std::common_type_t<Type1, Type2>>;     \
+  template auto Matrix<Type1>::operator*(const Matrix<Type2> &) const -> Matrix<std::common_type_t<Type1, Type2>>;     \
+  template auto Matrix<Type1>::operator*(const Type2 &) const -> Matrix<std::common_type_t<Type1, Type2>>;             \
+  template auto Matrix<Type1>::element_wise_product(const Matrix<Type2> &) const                                       \
+      -> Matrix<std::common_type_t<Type1, Type2>>;                                                                     \
+  template auto operator*(const Type1 &, const Matrix<Type2> &)->Matrix<std::common_type_t<Type1, Type2>>;
 
-
-template<typename T = double>
-class Matrix {
+template <typename T = double> class Matrix {
 private:
-	unsigned int m_row_size;
-	unsigned int m_col_size;
-	std::vector<std::vector<T>> m_elements;
+  unsigned int m_row_size;
+  unsigned int m_col_size;
+  std::vector<std::vector<T>> m_elements;
 
 public:
-	explicit Matrix(const unsigned int row_size, const unsigned int col_size);
-	explicit Matrix(const std::vector<std::vector<T>>& elements);
+  explicit Matrix(const unsigned int row_size, const unsigned int col_size);
+  explicit Matrix(const std::vector<std::vector<T>> &elements);
 
-	template<typename U>
-	Matrix(const Matrix<U>& matrix);
+  template <typename U> Matrix(const Matrix<U> &matrix);
 
-	unsigned int get_row_size() const;
-	unsigned int get_col_size() const;
+  unsigned int get_row_size() const;
+  unsigned int get_col_size() const;
 
-	std::vector<T>& operator[](const unsigned int index);
-	const std::vector<T>& operator[](const unsigned int index) const;
+  std::vector<T> &operator[](const unsigned int index);
+  const std::vector<T> &operator[](const unsigned int index) const;
 
-	template<typename U>
-	auto operator+(const Matrix<U>& matrix) const->Matrix<std::common_type_t<T, U>>;
+  template <typename U> auto operator+(const Matrix<U> &matrix) const -> Matrix<std::common_type_t<T, U>>;
 
-	template<typename U>
-	auto operator-(const Matrix<U>& matrix) const->Matrix<std::common_type_t<T, U>>;
+  template <typename U> auto operator-(const Matrix<U> &matrix) const -> Matrix<std::common_type_t<T, U>>;
 
-	template<typename U>
-	auto operator*(const Matrix<U>& matrix) const->Matrix<std::common_type_t<T, U>>;
+  template <typename U> auto operator*(const Matrix<U> &matrix) const -> Matrix<std::common_type_t<T, U>>;
 
-	template<typename U>
-	auto operator*(const U& factor) const->Matrix<std::common_type_t<T, U>>;
+  template <typename U> auto operator*(const U &factor) const -> Matrix<std::common_type_t<T, U>>;
 
-	template<typename U>
-	auto element_wise_product(const Matrix<U>& matrix) const->Matrix<std::common_type_t<T, U>>;
+  template <typename U> auto element_wise_product(const Matrix<U> &matrix) const -> Matrix<std::common_type_t<T, U>>;
 
-	Matrix<T> transpose() const;
+  Matrix<T> transpose() const;
 
-	//Matrix<double> inverse();
-	//double determinant();
+  // Matrix<double> inverse();
+  // double determinant();
 
-	void p() {
-		for (auto& row : this->m_elements) {
-			for (auto& col : row) {
-				std::cout << col << " ";
-			}
-			std::cout << "\n";
-		}
-	}
+  void p() {
+    for (auto &row : this->m_elements) {
+      for (auto &col : row) {
+        std::cout << col << " ";
+      }
+      std::cout << "\n";
+    }
+  }
 };
 
-template<typename T, typename U>
-auto operator*(const U& l_factor, const Matrix<T>& r_factor)->Matrix<std::common_type_t<T, U>>;
+template <typename T, typename U>
+auto operator*(const U &l_factor, const Matrix<T> &r_factor) -> Matrix<std::common_type_t<T, U>>;
 
 #endif // MATRIX_H
