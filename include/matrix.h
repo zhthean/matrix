fc91db6b9337e827d7f4e62ac9eb6e707cc0f0a3
ZH <106545838+zhthean@users.noreply.github.com>
2025-03-30 23:32:20 +0800

    More methods with tests (#4)
    * More matrix methods
    
    * Add tests and dependency

--- include/matrix.h
+++ include/matrix.h
@@ -2,6 +2,7 @@
 #define MATRIX_H
 
 #include <iostream>
+#include <string>
 #include <type_traits>
 #include <vector>
 
@@ -14,7 +15,8 @@
   template auto Matrix<Type1>::operator*(const Type2 &) const -> Matrix<std::common_type_t<Type1, Type2>>;             \
   template auto Matrix<Type1>::element_wise_product(const Matrix<Type2> &) const                                       \
       -> Matrix<std::common_type_t<Type1, Type2>>;                                                                     \
-  template auto operator*(const Type1 &, const Matrix<Type2> &)->Matrix<std::common_type_t<Type1, Type2>>;
+  template auto operator*(const Type1 &, const Matrix<Type2> &)->Matrix<std::common_type_t<Type1, Type2>>;             \
+  template bool Matrix<Type1>::operator==(const Matrix<Type2> &) const;
 
 template <typename T = double> class Matrix {
 private:
@@ -23,10 +25,19 @@ private:
   std::vector<std::vector<T>> m_elements;
 
 public:
+  Matrix();
+
+  Matrix(const Matrix<T> &other) = default;
+  Matrix<T> &operator=(const Matrix<T> &other) = default;
+  Matrix(Matrix<T> &&other) noexcept = default;
+  Matrix<T> &operator=(Matrix<T> &&other) noexcept = default;
+
+  ~Matrix() = default;
+
   explicit Matrix(const unsigned int row_size, const unsigned int col_size);
   explicit Matrix(const std::vector<std::vector<T>> &elements);
 
-  template <typename U> Matrix(const Matrix<U> &matrix);
+  template <typename U> Matrix(const Matrix<U> &other);
 
   unsigned int get_row_size() const;
   unsigned int get_col_size() const;
@@ -46,20 +57,36 @@ public:
 
   Matrix<T> transpose() const;
 
-  // Matrix<double> inverse();
-  // double determinant();
+  double determinant() const;
+  Matrix<double> inverse() const;
 
-  void p() {
-    for (auto &row : this->m_elements) {
-      for (auto &col : row) {
-        std::cout << col << " ";
-      }
-      std::cout << "\n";
-    }
-  }
+  void swap(const unsigned int chosen_index, const unsigned int swapped_index, const char axis = "r");
+
+  template <typename U> bool operator==(const Matrix<U> &matrix) const;
 };
 
 template <typename T, typename U>
 auto operator*(const U &l_factor, const Matrix<T> &r_factor) -> Matrix<std::common_type_t<T, U>>;
+
+template <typename T> std::ostream &operator<<(std::ostream &os, const Matrix<T> &matrix) {
+  std::string message = "Matrix([";
+
+  for (unsigned int row = 0; row < matrix.get_row_size(); row++) {
+    message += "[";
+    for (unsigned int col = 0; col < matrix.get_col_size(); col++) {
+      message += std::to_string(matrix[row][col]);
+      if (col != matrix.get_col_size() - 1) {
+        message += ", ";
+      }
+    }
+
+    message += row == matrix.get_row_size() - 1 ? "]" : "], ";
+  }
+
+  message += "])";
+  os << message;
+
+  return os;
+}
 
 #endif // MATRIX_H
---
+++ include/matrix_exceptions.h
@@ -0,0 +1,37 @@
+#ifndef MATRIX_EXCEPTIONS_H
+#define MATRIX_EXCEPTIONS_H
+
+#include <stdexcept>
+#include <string>
+
+class MatrixExceptions : public std::runtime_error {
+public:
+  explicit MatrixExceptions(const std::string &msg) : std::runtime_error(msg) {}
+};
+
+class InvalidMatrixDimensionException : public MatrixExceptions {
+public:
+  explicit InvalidMatrixDimensionException(const std::string &msg) : MatrixExceptions(msg) {}
+};
+
+class MatrixMismatchException : public MatrixExceptions {
+public:
+  explicit MatrixMismatchException(const std::string &msg) : MatrixExceptions(msg) {}
+};
+
+class MatrixNonSquareException : public MatrixExceptions {
+public:
+  explicit MatrixNonSquareException(const std::string &msg) : MatrixExceptions(msg) {}
+};
+
+class MatrixSingularException : public MatrixExceptions {
+public:
+  explicit MatrixSingularException(const std::string &msg) : MatrixExceptions(msg) {}
+};
+
+class MatrixNonInvertibleException : public MatrixExceptions {
+public:
+  explicit MatrixNonInvertibleException(const std::string &msg) : MatrixExceptions(msg) {}
+};
+
+#endif // MATRIX_EXCEPTIONS_H
