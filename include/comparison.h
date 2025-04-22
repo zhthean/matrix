50b0c2dce3f7a32f859b5b36e1a1b53185d8e1ff
ZH <106545838+zhthean@users.noreply.github.com>
2025-04-22 22:53:10 +0800

    include vector class and enhance cmake (#7)

--- include/comparison.h
+++ include/comparison.h
@@ -2,7 +2,6 @@
 #define COMPARISON_H
 
 #include <cmath>
-#include <iostream>
 
 template <typename T, typename U> bool numeric_almost_equal(T left_num, U right_num, double epsilon = 1e-9) {
   if constexpr (std::is_integral_v<T> && std::is_integral_v<U>) {
